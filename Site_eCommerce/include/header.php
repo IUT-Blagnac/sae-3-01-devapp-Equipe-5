18a565c225b3b821e558793b173b776a7c8b24b6
Damien_lt <125353264+DamienZoldyck@users.noreply.github.com>
2023-11-27 15:41:06 +0100

    Version 1 header.php
    1ère version du header.php

---
+++ Site_eCommerce/include/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MalyArt</title>
+    <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+    <header>
+            <div>
+                <img id='texteMalyart' src="malyart.png">
+                <img id='logoMalyart' src="logoRond.png" width=130px height=130px >
+            </div>
+            <nav>
+                <a href="index.php"> Accueil</a></li>
+                <a href="#"> Nouveautés</a></li>
+                <a href="#"> Promotions</a></li>
+                <a href="#"> Connexion</a></li>
+                <a href="#"> Mon panier</a></li>
+            </nav>
+    </header>
+    
+</body>
+</html>
